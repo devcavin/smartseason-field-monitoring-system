3bdd6421ac666de3151c8761c79d37cee5e105ea
Cavin <cavinlarry001@gmail.com>
2026-04-19 09:05:49 +0300

    Initial commit

--- src/hackerrank/Arrays.kt
+++
@@ -1,9 +0,0 @@
-package hackerrank
-
-fun main() {
-    val n = readLine()!!.trim().toInt()
-
-    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
-    require(arr.size == n)
-    println(arr.reversed().joinToString(" "))
-}
